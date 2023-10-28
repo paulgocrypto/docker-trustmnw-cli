e38965b97b15d06c8242fb2523671db3c4a38077
PaulGoCrypto <paul@trustmnw.paulgroeneweg.nl>
2023-10-28 14:36:15 +0200

    add monit config + update index.php check

--- data/web/index.php
+++ data/web/index.php
@@ -1,16 +1,35 @@
-<?php 
+<?php
+/*
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+*/
 
-$json = file_get_contents(__DIR__.'/../tmp/trustmnw-stats.json');
-
-$data = json_decode($json, true);
-
-$connected  = $data['connected']??false;
+//$json         = file_get_contents(__DIR__ . '/../tmp/trustmnw-stats.json');
+$json           = file_get_contents('/storage/trustmnw/data/tmp/trustmnw-stats.json');
+$data           = json_decode($json, true);
+$connected      = $data['connected'] ?? false;
+$currentDate    = new DateTime('now');
+$lastReportDate = new DateTime($data['lastReportDate']);
+$status         = 'Connected';
+$link           = '<br /><a href="http://trustmnw.paulgroeneweg.nl:2812/">Monit</a>';
 
 if ( $connected !== true){
 
-	header("HTTP/1.1 404 Not Found");
+    header("HTTP/1.1 404 Not Found");
+    $status = 'Disconnected';
 
-	echo 'Disconnected';
+}elseif ( $lastReportDate < $currentDate->modify('-5 MINUTES') ) {
+
+    header("HTTP/1.1 404 Not Found");
+    $status = 'Timed out';
+
 }else{
-	echo 'Connected: '.$data['lastReportDate'];
+
+    $link = null;
 }
+
+echo '<h1>'.$status.'</h1> ';
+echo $lastReportDate->format('d-m-Y H:i:s');
+echo $link;
+
